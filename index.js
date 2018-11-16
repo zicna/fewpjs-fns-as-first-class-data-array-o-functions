635d4a1dc978b3e3d4831c84fe4e9d9b2c7dd108
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-11-16 15:50:42 -0500

    Updates solution

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
-var monday, tuesday, wednesday, thursday, friday;
-
-function runFiveMiles() {
-  console.log('Go for a five-mile run');
-  return "Go for a five-mile run"
-}
-function liftWeights() {
-  console.log('Pump iron');
-  return "Pump iron"
-}
-function swimFortyLaps() {
-  console.log('Swim 40 laps');
-  return "Swim 40 laps"
+var wakeDog = function(dogName="Byron", dogBreed="poodle") {
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return `Wake ${dogName} the ${dogBreed}`
 }
 
-function exerciseRoutine(postRunActivity) {
-  var output = [runFiveMiles(), postRunActivity()];
-  var output1 = output[0].toString
-  var output2= output[1].toString
-  return `${output[0]} ${output[1]}`
+var leashDog = function(dogName="Byron", dogBreed="poodle") {
+    console.log(`Leash ${dogName} the ${dogBreed}`);
+    return `Leash ${dogName} the ${dogBreed}`
 }
 
-monday = thursday = function() {
-   exerciseRoutine(liftWeights);
+var walkToPark = function(dogName="Byron", dogBreed="poodle") {
+    console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+    return `Walk to the park with ${dogName} the ${dogBreed}`
 }
 
-tuesday = friday = function() {
-    exerciseRoutine(swimFortyLaps);
+var throwFrisbee = function(dogName="Byron", dogBreed="poodle") {
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`
 }
 
-wednesday = function() {
-    exerciseRoutine(runFiveMiles);
+var walkHome = function(dogName="Byron", dogBreed="poodle") {
+    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+    return `Walk home with ${dogName} the ${dogBreed}`
 }
 
-const functionsArray = [monday,tuesday,wednesday,thursday,friday]
+var unleashDog = function(dogName="Byron", dogBreed="poodle") {
+    console.log(`Unleash ${dogName} the ${dogBreed}`);
+    return `Unleash ${dogName} the ${dogBreed}`
+}
 
-function printRoutine() {
+const routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
+
+function exerciseDog() {
     for (var value of functionsArray) {
-        console.log(value.name + ":")
+        console.log(value)
         return value()
     }
 }
