c77bace8e67b987f3aa8761be6bb9d5f83e5ad79
Steven G. Harms <steven.harms@flatironschool.com>
2019-10-03 13:32:27 -0400

    Do not use fn declarations

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-var wakeDog = function(dogName="Byron", dogBreed="poodle") {
+function wakeDog(dogName="Byron", dogBreed="poodle") {
   console.log(`Wake ${dogName} the ${dogBreed}`);
   return `Wake ${dogName} the ${dogBreed}`
 }
 
-var leashDog = function(dogName="Byron", dogBreed="poodle") {
+function leashDog(dogName="Byron", dogBreed="poodle") {
   console.log(`Leash ${dogName} the ${dogBreed}`);
   return `Leash ${dogName} the ${dogBreed}`
 }
 
-var walkToPark = function(dogName="Byron", dogBreed="poodle") {
+function walkToPark(dogName="Byron", dogBreed="poodle") {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
   return `Walk to the park with ${dogName} the ${dogBreed}`
 }
 
-var throwFrisbee = function(dogName="Byron", dogBreed="poodle") {
+function throwFrisbee(dogName="Byron", dogBreed="poodle") {
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
   return `Throw the frisbee for ${dogName} the ${dogBreed}`
 }
 
-var walkHome = function(dogName="Byron", dogBreed="poodle") {
+function walkHome(dogName="Byron", dogBreed="poodle") {
   console.log(`Walk home with ${dogName} the ${dogBreed}`);
   return `Walk home with ${dogName} the ${dogBreed}`
 }
 
-var unleashDog = function(dogName="Byron", dogBreed="poodle") {
+function unleashDog(dogName="Byron", dogBreed="poodle") {
   console.log(`Unleash ${dogName} the ${dogBreed}`);
   return `Unleash ${dogName} the ${dogBreed}`
 }
